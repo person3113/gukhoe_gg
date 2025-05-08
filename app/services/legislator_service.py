c7475cf99b4e7d979251c4fc3a85c12d3afa9439
person3113 <person3113@gmail.com>
2025-05-08 19:02:49 +0900

    /champions 화면에서 선거구 옵션 안 나오는 현상 수정

--- app/services/legislator_service.py
+++ app/services/legislator_service.py
@@ -7,7 +7,7 @@ from app.models.committee import CommitteeHistory
 
 def get_filter_options(db: Session) -> Dict[str, List[str]]:
     """
-    필터 옵션(정당, 선거구, 초선/재선) 데이터 조회
+    필터 옵션(정당, 위원회, 초선/재선, 선거구) 데이터 조회
     
     Args:
         db: 데이터베이스 세션
@@ -17,21 +17,26 @@ def get_filter_options(db: Session) -> Dict[str, List[str]]:
     """
     # 정당 목록 조회
     parties = db.query(Legislator.poly_nm).distinct().all()
-    party_list = [party[0] for party in parties]
+    party_list = [party[0] for party in parties if party[0]]
     
-    # 선거구 목록 조회
-    districts = db.query(Legislator.orig_nm).distinct().all()
-    district_list = [district[0] for district in districts]
+    # 위원회 목록 조회
+    committees = db.query(Legislator.cmit_nm).distinct().all()
+    committee_list = [committee[0] for committee in committees if committee[0]]  # None 값 제외
     
     # 초선/재선 목록 조회
     terms = db.query(Legislator.reele_gbn_nm).distinct().all()
-    term_list = [term[0] for term in terms]
+    term_list = [term[0] for term in terms if term[0]]
+    
+    # 선거구 목록 조회
+    districts = db.query(Legislator.orig_nm).distinct().all()
+    district_list = [district[0] for district in districts if district[0]]  # None 값 제외
     
     # 필터 옵션 딕셔너리 생성
     filter_options = {
         "parties": party_list,
-        "districts": district_list,
-        "terms": term_list
+        "committees": committee_list,
+        "terms": term_list,
+        "districts": district_list  # 선거구 목록 추가
     }
     
     return filter_options
@@ -223,36 +228,3 @@ def get_legislator_committee_history(db: Session, legislator_id: int) -> List[Di
         })
     
     return result
-
-
-# 활동랭킹 카테고리별 필터 옵션
-def get_filter_options(db: Session) -> Dict[str, List[str]]:
-    """
-    필터 옵션(정당, 위원회, 초선/재선) 데이터 조회
-    
-    Args:
-        db: 데이터베이스 세션
-    
-    Returns:
-        필터 옵션 딕셔너리
-    """
-    # 정당 목록 조회
-    parties = db.query(Legislator.poly_nm).distinct().all()
-    party_list = [party[0] for party in parties if party[0]]
-    
-    # 위원회 목록 조회
-    committees = db.query(Legislator.cmit_nm).distinct().all()
-    committee_list = [committee[0] for committee in committees if committee[0]]  # None 값 제외
-    
-    # 초선/재선 목록 조회
-    terms = db.query(Legislator.reele_gbn_nm).distinct().all()
-    term_list = [term[0] for term in terms if term[0]]
-    
-    # 필터 옵션 딕셔너리 생성
-    filter_options = {
-        "parties": party_list,
-        "committees": committee_list,
-        "terms": term_list
-    }
-    
-    return filter_options
